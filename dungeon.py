f835231387551f19e9fb06c819da3de452804ced
Andrew Butler <158104471+AjButler03@users.noreply.github.com>
2025-05-22 13:11:31 -0500

    v01 Continuing Progress

--- dungeon.py
+++ dungeon.py
@@ -2,8 +2,13 @@ import random
 from enum import Enum
 from utility import exp_chancetime
 
+min_room_h = 3
+min_room_w = 3
+min_roomc = 1
+
+
 class dungeon:
-    
+
     # Enum to dictate the terrain types of a dungeon
     class terrain(Enum):
         debug = 0
@@ -20,7 +25,7 @@ class dungeon:
             self.origin_r = origin_r
             self.origin_c = origin_c
             self.rsize_h = rsize_h
-            self.rsize_c = rsize_c
+            self.rsize_w = rsize_c
 
     class staircase:
         # Staircase Constructor
@@ -40,25 +45,47 @@ class dungeon:
         self.rmap = [[255] * self.width for _ in range(self.height)]
         # Declaring the terrain map for the dungeon
         self.tmap = [[self.terrain.debug] * self.width for _ in range(self.height)]
-        
+
+    # Boolean function; checks if point is within immutable outer border of dungeon
     def valid_point(self, r, c):
-        # TODO
-        return
-        
+        if r < 1 or c < 1 or r > self.height - 2 or c > self.width - 2:
+            return False
+        else:
+            return True
+
     # Attempts to place a room into the dungeon terrain map
     def _place_room(self, room):
-        # TODO
-        # copy of the terrain map to easily revert changes if room cannot be placed
-        tmap_copy = self.tmap.deepcopy()
         r = room.origin_r
         c = room.origin_c
         last_r = r + room.rsize_h
         last_c = c + room.rsize_w
-        
-        while (r <  last_r):
-            while (c < last_c):
-                return
-                
+
+        if self.tmap[r][c] == self.terrain.debug:
+            # Room origin conflicts with another room
+            return False
+
+        # copy of the terrain & rock maps to easily revert changes if room cannot be placed
+        rmap_copy = self.rmap.deepcopy()
+        tmap_copy = self.tmap.deepcopy()
+
+        while r < last_r:
+            while c < last_c:
+                if not self.valid_point(
+                    r, 
+                ):
+                    # Out of bounds
+                    return False
+                elif (
+                    tmap_copy[r + 1][c] == self.terrain.floor
+                    or tmap_copy[room.origin_r - 1][c] == self.terrain.floor
+                    or tmap_copy[r][room.origin_c - 1] == self.terrain.floor
+                ):
+                    # Room is either going to be adjacent to another room, or run into another room
+                    return False
+                else:
+                    # Claim as room cell on dungeon maps
+                    tmap_copy[r][c] = self.terrain.floor
+                    rmap_copy[r][r] = 0
 
     # Generates dungeon rock hardness map, necessary for Dijkstra path generation + NPC pathfinding
     def _generate_rockmap(self):
@@ -70,41 +97,51 @@ class dungeon:
     # Cannot be called before generating the rockmap
     def _generate_terrain(self):
         # Creating rooms; at least 1
-        while (self.roomc < 1) or (exp_chancetime(self.roomc)):
-            new_room = self.room(
-                (random.random() % self.height),
-                (random.random() % self.width),
-                (random.random() % (self.height / 2)),
-                (random.random() % (self.width / 2)),
-            )
-            
+        attemptc = 0  # To keep track of the number of room placement attempts
+        # Right now it's just hardcoded to attempt 25 times (at least one success), but that could be changed later.
+        while (self.roomc < min_roomc) and (attemptc < 25):
+            if (self.roomc == 0) or (exp_chancetime(self.roomc)):
+                new_room = self.room(
+                    int(random.randint(1, self.height - 1)),
+                    int(random.randint(1, self.width - 1)),
+                    int(random.randint(min_room_h, self.height // 2)),
+                    int(random.randint(min_room_w, self.width // 2)),
+                )
+                if self._place_room(new_room):
+                    self.roomc += 1
+            attemptc += 1
+            print(attemptc)
+            print(self.roomc)
+
     def print_terrain(self):
         for r in range(self.height):
             for c in range(self.width):
                 t_type = self.tmap[r][c]
-                if (t_type == self.terrain.floor):
+                if t_type == self.terrain.floor:
                     print(".", end="")
-                elif (t_type == self.terrain.stair):
+                elif t_type == self.terrain.stair:
                     print("<", end="")
-                elif (t_type == self.terrain.stdrock):
+                elif t_type == self.terrain.stdrock:
                     print(" ", end="")
-                elif (t_type == self.terrain.immrock):
-                    print("X", end="") # will want to be a proper border later
+                elif t_type == self.terrain.immrock:
+                    print("X", end="")  # will want to be a proper border later
                 else:
-                    print("!", end="") # issue flag
-            
-            print("") # Newline for end of row
-    
+                    print("!", end="")  # issue flag
+
+            print("")  # Newline for end of row
+
     # Generates a random dungeon
     def generate_dungeon(self):
-        # TODO
         self._generate_rockmap()
         self._generate_terrain()
         return
 
+
 def main():
     d = dungeon(21, 80)
+    d.generate_dungeon()
     d.print_terrain()
+
 
 if __name__ == "__main__":
     main()
