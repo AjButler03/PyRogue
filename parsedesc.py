1238828f1b2e3634f97ae84d0c504b24f602796d
Andrew Butler <158104471+AjButler03@users.noreply.github.com>
2025-07-13 14:19:51 -0500

    v06 Just saving progress yet again

--- parsedesc.py
+++ parsedesc.py
@@ -24,7 +24,8 @@ def parse_monsters(monster_type_list) -> bool:
         
         # Header matches; now read the monster descriptions
         curr_line += 1
-        while curr_line < len(file_lines):
+        line_count = len(file_lines)
+        while curr_line < line_count:
             line = file_lines[curr_line].strip()
             # Check for beginning of monster definition
             if line == "BEGIN MONSTER":
@@ -38,9 +39,20 @@ def parse_monsters(monster_type_list) -> bool:
                         # Parse name field
                         name = line[0:4]
                     elif line.startswith("DESC"):
-                        # Parse text description field
-                        # TODO
-                        pass
+                        # Parse text description field; read until "." found
+                        curr_line += 1 # move to the line that starts the actual description
+                        line = file_lines[curr_line]
+                        desc = ""
+                        while line != ".":
+                            # Check to make sure that we don't try and read past the end of the file
+                            if curr_line >= line_count:
+                                # Did not find termination ".", so the file is not formatted correctly. Return False.
+                                return False
+                            else:
+                                # Read line; add to desc field
+                                desc += line
+                                curr_line += 1
+                                line = file_lines[curr_line]
                     elif line.startswith("COLOR"):
                         # Parse color field(s)
                         # TODO
